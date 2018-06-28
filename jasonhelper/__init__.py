fc2346d8450a9174f412f7aa144d6b76de7ee011
Jason Koh <bk7749@gmail.com>
2018-06-27 17:24:58 -0700

    add chunks

--- jasonhelper/__init__.py
+++ jasonhelper/__init__.py
@@ -43,15 +43,20 @@ class bidict(dict):
         self.inverse = {}
         for key, value in self.items():
             self.inverse.setdefault(value,[]).append(key)
-            
+
     def __setitem__(self, key, value):
         if key in self:
             self.inverse[self[key]].remove(key)
         super(bidict, self).__setitem__(key, value)
         self.inverse.setdefault(value,[]).append(key)
-        
+
     def __delitem__(self, key):
         self.inverse.setdefault(self[key],[]).remove(key)
         if self[key] in self.inverse and not self.inverse[self[key]]:
             del self.inverse[self[key]]
         super(bidict, self).__delitem__(key)
+
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
