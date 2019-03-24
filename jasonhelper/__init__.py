a10785ea04aea1cbd0026b042ade7c186d93fd01
Jason Koh <bk7749@gmail.com>
2019-03-24 14:42:09 -0700

    update

--- jasonhelper/__init__.py
+++ jasonhelper/__init__.py
@@ -32,13 +32,11 @@ def rolling_window(l, w_size):
     for i in range(len(l)-w_size+1):
         yield [l[i+o] for o in range(w_size)]
 
-def striding_window(l, w_size):
+def striding_windows(l, w_size):
     curr_idx = 0
-    while True:
+    while curr_idx < len(l):
         yield l[curr_idx:curr_idx + w_size]
         curr_idx += w_size
-        if curr_idx > len(l):
-            break
 
 def check_and_create_dir(dir_path):
     if not os.path.exists(dir_path):
